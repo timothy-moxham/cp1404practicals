aed4c748d9568e5fa6b23955368a06f6f012b1aa
timothyy <timothy.moxham@my.jcu.edu.au>
2025-09-26 18:02:44 +1000

    Provide module and function docstrings

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,16 +1,24 @@
+"""
+CP1404/CP5632 - Practical
+Program to get a valid password and print it as asterisks
+"""
+
 MINIMUM_LENGTH = 5
 
 
 def main():
+    """Get and print password as asterisks."""
     password = get_password()
     print_asterisks(password)
 
 
-def print_asterisks(password):
-    print(len(password) * "*")
+def print_asterisks(text):
+    """Print text as asterisks."""
+    print(len(text) * "*")
 
 
 def get_password():
+    """Get a password that meets a minimum length."""
     password = input("Password: ")
     while len(password) < MINIMUM_LENGTH:
         print("Error")
