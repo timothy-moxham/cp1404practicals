631c83040f8f537d4d311049e8877e3676aae527
timothyy <timothy.moxham@my.jcu.edu.au>
2025-10-02 00:47:28 +1000

    Complete third subtask of Files practical task

--- prac_03/files.py
+++ prac_03/files.py
@@ -3,14 +3,21 @@ CP1404/CP5632 - Practical
 Files
 """
 
-# 1
+# 1.
 name = input("Name: ")
 out_file = open("data.txt", "w")
 print(name, file=out_file)
 out_file.close()
 
-# 2
+# 2.
 in_file = open("data.txt", "r")
 name = in_file.readline().strip()
 print(f"Hi {name}!")
 in_file.close()
+
+# 3.
+with open("numbers.txt", "r") as in_file:
+    first_number = int(in_file.readline())
+    second_number = int(in_file.readline())
+total = first_number + second_number
+print(total)
