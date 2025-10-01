77b46870af4791b6f18bc1cd6cd0d7f3d9c131a5
timothyy <timothy.moxham@my.jcu.edu.au>
2025-10-02 00:18:00 +1000

    Complete second subtask of Files practical task

--- prac_03/files.py
+++ prac_03/files.py
@@ -8,3 +8,9 @@ name = input("Name: ")
 out_file = open("data.txt", "w")
 print(name, file=out_file)
 out_file.close()
+
+# 2
+in_file = open("data.txt", "r")
+name = in_file.readline().strip()
+print(f"Hi {name}!")
+in_file.close()
