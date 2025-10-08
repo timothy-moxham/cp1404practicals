a3fda972a455a4f6110edfa2301e54aa23064b2c
timothyy <timothy.moxham@my.jcu.edu.au>
2025-10-08 12:35:35 +1000

    Implement username authoriser

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -11,3 +11,13 @@ print(f"The first number is {numbers[0]}\n"
       f"The smallest number is {min(numbers)}\n"
       f"The largest number is {max(numbers)}\n"
       f"The average of the numbers is {sum(numbers) / len(numbers)}")
+
+usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface', 'BaseStdIn',
+             'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
+
+username = input("Username: ")
+
+if username in usernames:
+    print("Access granted")
+else:
+    print("Access denied")
