6f8c5df199e4ece04973b3ca7e8a25e409cc897d
timothyy <timothy.moxham@my.jcu.edu.au>
2025-11-07 18:14:11 +1000

    Create save_projects function and implement into main function

--- prac_07/project_management.py
+++ prac_07/project_management.py
@@ -19,17 +19,20 @@ DEFAULT_FILE = "projects.txt"
 def main():
     """..."""
     print("Welcome to Pythonic Project Management")
-    projects = load_projects(DEFAULT_FILE)
-    print(f"Loaded {len(projects)} projects from {DEFAULT_FILE}")
+    filename = DEFAULT_FILE
+    projects = load_projects(filename)
+    print(f"Loaded {len(projects)} projects from {filename}")
     print(MENU)
     choice = input(">>> ").upper()
     while choice != "Q":
         if choice == "L":
             filename = get_valid_filename()
-            projects = load_projects(DEFAULT_FILE)
-            print(f"Loaded {len(projects)} from {filename}")
+            projects = load_projects(filename)
+            print(f"Loaded {len(projects)} projects from {filename}")
         elif choice == "S":
-            pass
+            save_filename = input("Filename: ")
+            save_projects(save_filename, projects)
+            print(f"Saved {len(projects)} projects to {save_filename}")
         elif choice == "D":
             pass
         elif choice == "F":
@@ -52,6 +55,15 @@ def load_projects(file):
             project = line.strip().split("\t")
             projects.append(Project(project[0], project[1], int(project[2]), float(project[3]), int(project[4])))
         return projects
+
+
+def save_projects(file, projects):
+    """..."""
+    with open(file, "w") as out_file:
+        print("Name	Start Date\tPriority\tCost Estimate\tCompletion Percentage", file=out_file)
+        for project in projects:
+            print(f"{project.name}\t{project.start_date}\t{project.priority}\t{project.cost_estimate}\t"
+                  f"{project.completion_percentage}", file=out_file)
 
 
 def get_valid_filename():
