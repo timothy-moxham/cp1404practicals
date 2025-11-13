98e533b78b434f85328b28a3509b9f0e7fa4bcac
timothyy <timothy.moxham@my.jcu.edu.au>
2025-11-13 20:08:40 +1000

    Implement handle_convert function

--- prac_08/convert_miles_km.py
+++ prac_08/convert_miles_km.py
@@ -6,12 +6,17 @@ from kivy.app import App
 from kivy.lang import Builder
 from kivy.core.window import Window
 
+
 class ConvertMilesApp(App):
     def build(self):
         Window.size = (600, 300)
         self.title = "Convert Miles to Kilometres"
         self.root = Builder.load_file('convert_miles_km.kv')
         return self.root
+
+    def handle_convert(self, value):
+        result = float(value) * 1.60934
+        self.root.ids.output_label.text = str(result)
 
 
 ConvertMilesApp().run()
