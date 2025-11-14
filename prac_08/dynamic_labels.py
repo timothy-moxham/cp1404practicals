2ea4131a849aca8ebcf264aa953f8cbc0fcb1aee
timothyy <timothy.moxham@my.jcu.edu.au>
2025-11-14 13:31:26 +1000

    Finish Dynamic Kivy Widgets practical task

---
+++ prac_08/dynamic_labels.py
@@ -0,0 +1,25 @@
+
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.uix.label import Label
+
+
+class DynamicLabelsApp(App):
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.names = ["Timothy", "John", "Aaron", "Zack"]
+
+    def build(self):
+        self.title = "Dynamic Labels App"
+        self.root = Builder.load_file('dynamic_labels.kv')
+        self.create_widgets()
+        return self.root
+
+    def create_widgets(self):
+        for name in self.names:
+            temp_label = Label(text=name)
+            self.root.ids.main.add_widget(temp_label)
+
+
+DynamicLabelsApp().run()
