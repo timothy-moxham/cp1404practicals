1bc167f767c14cbf1fddac17f4bfb1a2241d4f69
timothyy <timothy.moxham@my.jcu.edu.au>
2025-11-22 17:34:23 +1000

    Test taxi class

---
+++ prac_09/taxi_test.py
@@ -0,0 +1,21 @@
+"""
+CP1404/CP5632 Practical
+Program to test taxi class
+"""
+
+from taxi import Taxi
+
+
+def main():
+    """Test taxi class"""
+    my_taxi = Taxi("Prius 1", 100, 1.23)
+
+    my_taxi.drive(40)
+    print(f"{my_taxi}, Fare: ${my_taxi.get_fare()}")
+
+    my_taxi.start_fare()
+    my_taxi.drive(100)
+    print(f"{my_taxi}, Fare: ${my_taxi.get_fare()}")
+
+
+main()
