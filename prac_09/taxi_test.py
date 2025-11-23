3360b93c2cf7415e86a2c9d78c9e73758327819d
Timothy Charles Moxham <timothy.moxham@my.jcu.edu.au>
2025-11-23 18:51:59 +1000

    Update Taxi class test program

--- prac_09/taxi_test.py
+++ prac_09/taxi_test.py
@@ -8,7 +8,7 @@ from taxi import Taxi
 
 def main():
     """Test taxi class."""
-    my_taxi = Taxi("Prius 1", 100, 1.23)
+    my_taxi = Taxi("Prius 1", 100)
 
     distance = my_taxi.drive(40)
     print(f"{my_taxi}, Fare: ${my_taxi.get_fare()}, expected ${distance * my_taxi.price_per_km}")
