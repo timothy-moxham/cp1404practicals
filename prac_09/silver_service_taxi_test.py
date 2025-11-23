2c2ebcf9a9493c23fb14562f8d3a2c37dca29b62
timothyy <timothy.moxham@my.jcu.edu.au>
2025-11-23 14:15:45 +1000

    Update Silver Service Taxi test program

--- prac_09/silver_service_taxi_test.py
+++ prac_09/silver_service_taxi_test.py
@@ -5,11 +5,14 @@ Program to test SilverServiceTaxi class
 
 from silver_service_taxi import SilverServiceTaxi
 
+KNOWN_VALUE = 48.80
+
+
 def main():
     """Test SilverServiceTaxi class."""
     premium_taxi = SilverServiceTaxi(2, "Premium Taxi", 18)
     premium_taxi.drive(18)
-    assert premium_taxi.get_fare() == 48.78  # No AssertionError raised with known, expected value
+    assert premium_taxi.get_fare() == 48.80  # No AssertionError raised with known, expected value
 
 
 main()
