c5a2d91f8bada79e0677deb6bca0669a1417388d
timothyy <timothy.moxham@my.jcu.edu.au>
2025-11-23 17:45:38 +1000

    Revert "Implement displaying of taxi info and final bill on program quit"
    This reverts commit 18674d6cfc8240ae356ccebc141658d41373ff9c.

--- prac_09/taxi_simulator.py
+++ prac_09/taxi_simulator.py
@@ -19,7 +19,6 @@ def main():
 
     while choice != "q":
         if choice == "c":
-            print("Taxis available:")
             current_taxi = choose_taxi(taxis)
         elif choice == "d":
             if current_taxi is None:
@@ -31,10 +30,6 @@ def main():
         print(f"Bill to date: ${bill:.2f}")
         print(MENU)
         choice = input(">>> ").lower()
-
-    print(f"Total trip cost: ${bill:.2f}")
-    print("Taxis are now:")
-    display_taxis(taxis)
 
 
 def choose_taxi(taxis):
@@ -49,6 +44,7 @@ def choose_taxi(taxis):
 
 def display_taxis(taxis):
     """"""
+    print("Taxis available:")
     for i, taxi in enumerate(taxis):
         print(f"{i} - {taxi}")
 
