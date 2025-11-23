16709f9a1f43a84a88c6db81201aa4b60c6c5d9b
timothyy <timothy.moxham@my.jcu.edu.au>
2025-11-23 17:45:40 +1000

    Revert "Add rounding to nearest 10c for get_fare method"
    This reverts commit 466ef94107089c754fcb8537104e9e1f744432ce.

--- prac_09/taxi.py
+++ prac_09/taxi.py
@@ -21,7 +21,7 @@ class Taxi(Car):
 
     def get_fare(self):
         """Return the price for the taxi trip."""
-        return round(self.price_per_km * self.current_fare_distance, 1)
+        return self.price_per_km * self.current_fare_distance
 
     def start_fare(self):
         """Begin a new fare."""
