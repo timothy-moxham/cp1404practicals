199bc6766336139e65f88454f07fa144658157f9
timothyy <timothy.moxham@my.jcu.edu.au>
2025-11-23 17:45:38 +1000

    Revert "Implement choose taxi functionality"
    This reverts commit 77efd1d72e46256cba6af20fa8b4436a32951739.

--- prac_09/taxi_simulator.py
+++ prac_09/taxi_simulator.py
@@ -11,14 +11,13 @@ MENU = "q)uit, c)hoose taxi, d)rive"
 def main():
     """"""
     taxis = [Taxi("Prius", 100), SilverServiceTaxi("Limo", 100, 2), SilverServiceTaxi("Hummer", 200, 4)]
-    current_taxi = None
     bill = 0
     print(MENU)
     choice = input(">>> ").lower()
 
     while choice != "q":
         if choice == "c":
-            current_taxi = choose_taxi(taxis)
+            pass
         elif choice == "d":
             pass
         else:
@@ -26,23 +25,6 @@ def main():
         print(f"Bill to date: ${bill:.2f}")
         print(MENU)
         choice = input(">>> ").lower()
-
-
-def choose_taxi(taxis):
-    """"""
-    display_taxis(taxis)
-    try:
-        taxi_choice = int(input("Choose taxi: "))
-        return taxis[taxi_choice]
-    except Exception as e:
-        print("Invalid taxi choice")
-
-
-def display_taxis(taxis):
-    """"""
-    print("Taxis available:")
-    for i, taxi in enumerate(taxis):
-        print(f"{i} - {taxi}")
 
 
 main()
