0e6601e92eb226c9f3795286de479a206e005e0c
timothyy <timothy.moxham@my.jcu.edu.au>
2025-11-23 17:45:37 +1000

    Revert "Delete prac_09/band.py"
    This reverts commit 8406ea2219ba9c3a1bc18e633791502899bdac43.

---
+++ prac_09/band.py
@@ -0,0 +1,25 @@
+"""
+CP1404/CP5632 Practical
+Band class
+"""
+
+
+class Band:
+    """Band class for storing details of a band."""
+
+    def __init__(self, name):
+        """Initialise a Band instance."""
+        self.name = name
+        self.musicians = []
+
+    def __str__(self):
+        """Return a string representation of a Band"""
+        return f"{self.name} ({", ".join(str(musician) for musician in self.musicians)})"
+
+    def add(self, musician):
+        """Append a musician to the band musicians list."""
+        return self.musicians.append(musician)
+
+    def play(self):
+        """Return string representation of musicians playing."""
+        return "\n".join(f"{musician.play()}" for musician in self.musicians)
