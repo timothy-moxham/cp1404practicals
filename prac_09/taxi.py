6943a813264843bea431c7aca272fc05d40188a4
timothyy <timothy.moxham@my.jcu.edu.au>
2025-11-23 17:45:42 +1000

    Revert "Change taxi class to use price_per_km as a class variable"
    This reverts commit ff368cfe3c5d6dd558a97734cf33dbd27a33a1c9.

--- prac_09/taxi.py
+++ prac_09/taxi.py
@@ -2,17 +2,16 @@
 CP1404/CP5632 Practical
 Car class
 """
-
 from prac_09.car import Car
 
 
 class Taxi(Car):
     """Specialised version of a Car that includes fare costs."""
-    price_per_km = 1.23
 
-    def __init__(self, name, fuel):
+    def __init__(self, name, fuel, price_per_km):
         """Initialise a Taxi instance, based on parent class Car."""
         super().__init__(name, fuel)
+        self.price_per_km = price_per_km
         self.current_fare_distance = 0
 
     def __str__(self):
