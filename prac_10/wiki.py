a07bdfb92172fd27aa657d78aee6a6601d77de6f
Timothy Charles Moxham <timothy.moxham@my.jcu.edu.au>
2025-11-27 21:16:57 +1000

    Refactor to use display_page function

--- prac_10/wiki.py
+++ prac_10/wiki.py
@@ -12,19 +12,24 @@ def main():
     """Get Wikipedia page title, and display information about that page."""
     title = input("Enter page title: ")
     while title != "":
-        try:
-            page = wikipedia.page(title, auto_suggest=False)
-            print(page.title)
-            print(wikipedia.summary(page, sentences=3))
-            print(page.url)
-            print()
-        except DisambiguationError:
-            print("We need a more specific title. Try one of the following, or a new search: ")
-            print(wikipedia.search(title), "\n")
-        except PageError:
-            print(f"Page id \"{title}\" does not match any pages. Try another id!\n")
+        display_page(title)
         title = input("Enter page title: ")
     print("Thank you.")
+
+
+def display_page(title: str):
+    """Display page title, summary, and url of a Wikipedia page."""
+    try:
+        page = wikipedia.page(title, auto_suggest=False)
+        print(page.title)
+        print(wikipedia.summary(page, sentences=3))
+        print(page.url)
+        print()
+    except DisambiguationError:
+        print("We need a more specific title. Try one of the following, or a new search: ")
+        print(wikipedia.search(title), "\n")
+    except PageError:
+        print(f"Page id \"{title}\" does not match any pages. Try another id!\n")
 
 
 main()
